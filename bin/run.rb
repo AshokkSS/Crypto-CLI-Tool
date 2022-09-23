71ea959f1af8e8634cc9f16bc55de16779ec0262
Ashokk S <ashokks@chillibean.com>
2022-09-23 11:49:31 +0100

    Makes call to api.
    Needs input validation for the crypto entered. There is input validation for the fiat currency, also if the user doesn't input a fiat value it defaults to gbp

--- bin/run.rb
+++ bin/run.rb
@@ -2,6 +2,7 @@ require_relative '../config/environment'
 require_relative '../lib/run'
 
 loading_menu
+API_URL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=gbp"
 quit = nil
 option_quit = nil
 show_logo
@@ -19,8 +20,26 @@ while !quit
         puts CLEAR
             while !option_quit
                 show_logo
-                puts "Here you can enter the Crypto you would like to view:"
-                input = gets.chomp
+                puts "Here you can enter the name of the Crypto you would like to price check: (Example: Bitcoin)"
+                crypto_input = gets.chomp
+                puts "Here you can enter the fiat currency you would like to use: (Example: GBP)"
+                fiat_input = gets.chomp
+                fiat_input = "gbp" if fiat_input.empty?
+                supported_currency = "https://api.coingecko.com/api/v3/simple/supported_vs_currencies"
+                uri = URI(supported_currency)
+                res = Net::HTTP.get_response(uri)
+                parsed_json = JSON.parse(res.body)
+                if parsed_json.include? fiat_input
+                    puts "Valid fiat input." 
+                else 
+                    puts "Invalid fiat input. Here is a list of valid Currencies you can input #{parsed_json}"
+                end
+                url = "https://api.coingecko.com/api/v3/simple/price?ids="
+                url << "#{crypto_input}&vs_currencies=#{fiat_input}&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true"
+                uri = URI(url)
+                res = Net::HTTP.get_response(uri)
+                parsed_json = JSON.parse(res.body)
+                puts res.body
                 option_result = prompt.select("Please select an option below.", exit_options, filter: true)
                 case option_result
                 when "Input_Another"
