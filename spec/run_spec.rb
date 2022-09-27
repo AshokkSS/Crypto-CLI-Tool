68933f66c0d666c9a62e122dc64106eeea81ec51
Ashokk S <ashokks@chillibean.com>
2022-09-27 14:25:58 +0100

    cleared run_spec

--- spec/run_spec.rb
+++ spec/run_spec.rb
@@ -4,23 +4,23 @@ require_relative '../config/environment'
 
 
 
-describe "#loading_menu" do
-  # expect{display_main_menu}.to eq('my message').to_stdout
-  # printed = capture_stdout do
-  #     display_main_menu
-  #   end
-    it 'displays menu options' do
-      #test_prompt = TTY::Prompt.new
+# describe "#loading_menu" do
+#   # expect{display_main_menu}.to eq('my message').to_stdout
+#   # printed = capture_stdout do
+#   #     display_main_menu
+#   #   end
+#     it 'displays menu options' do
+#       #test_prompt = TTY::Prompt.new
       
-      ping 
-        #expect(PROMPT).to receive(:select).with(%w(Price_Check Compare_Cryptos My_Portfolio Close))
-      expect(Net::HTTPSuccess).to receive(ping).and_return("correct")
+#       ping 
+#         #expect(PROMPT).to receive(:select).with(%w(Price_Check Compare_Cryptos My_Portfolio Close))
+#       expect(Net::HTTPSuccess).to receive(ping).and_return("correct")
 
       
     
       
-  end
-end
+#   end
+# end
 # context "#display_main_menu" do
 #   it 'should print "my message"' do
 #     printed = capture_stdout do
