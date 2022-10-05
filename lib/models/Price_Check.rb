10a41c400cc41980d72f07625125c30afa92eb91
Ashokk S <ashokks@chillibean.com>
2022-10-05 14:21:55 +0100

    now parses the data received and uses the users input when making api calls
    Tests also written for the api call.

--- lib/models/Price_Check.rb
+++ lib/models/Price_Check.rb
@@ -2,8 +2,6 @@ require_relative './Crypto_Api.rb'
 require_relative './Main_Menu.rb'
 require_relative '../../config/environment'
 
-
-
 class PriceCheck
     CRYPTOAPI = CryptoAPI.new
     #MAINMENU = MainMenu.new
@@ -18,6 +16,28 @@ class PriceCheck
         MainMenu.new.show_logo
         crypto_input = get_crypto_input
         fiat_input = get_fiat_input
+        puts CLEAR
+        MainMenu.new.show_logo
+        CRYPTOAPI.get_crypto_price(crypto_input,fiat_input)
+        display_table(crypto_input,fiat_input)
+    end
+
+    def display_table(crypto_input, fiat_input)
+        values = CRYPTOAPI.price_check_values
+        puts "Showing Live stats for: #{(crypto_input).upcase} in #{(fiat_input).upcase}"
+        if values[5].positive? == true
+            values [5] = "+#{values[5]}%".to_s.green
+            table = TTY::Table.new(["Price","Market Cap","24 Hours Volume"," 24 Hours Change"], [[values[2],values[3],values[4],values[5]]])
+        else
+            values [5] = "#{values[5]}%".to_s.red
+            table = TTY::Table.new(["Price","Market Cap","24 Hours Volume"," 24 Hours Change"], [[values[2],values[3],values[4],values[5]]])
+        end
+        puts table.render(:unicode,padding: [1,2,1,2])
+        puts "1 #{values[0]} is worth #{values[2]} #{values[1].upcase}."
+        puts "10 #{values[0]}'s are worth #{values[2]*10} #{values[1].upcase}."
+        puts "100 #{values[0]}'s are worth #{values[2]*100} #{values[1].upcase}."
+        puts "500 #{values[0]}'s are worth #{values[2]*500} #{values[1].upcase}."
+        puts "1000 #{values[0]}'s are worth #{values[2]*1000} #{values[1].upcase}."
     end
 
     def get_crypto_input 
